eeaa6f9c68802f827e4ce96f9291efa0d9dd7451
Анастасия Пакаева <staroxy63@gmail.com>
2022-08-08 12:41:00 +0300

    fix test

--- src/test/java/ru/netology/service/OrderTest.java
+++ src/test/java/ru/netology/service/OrderTest.java
@@ -1,15 +1,24 @@
 package ru.netology.service;
+
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class OrderTest {
+
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:9999");
+    }
+
     @Test
     void shouldSubmitRequest() {
-        open("http://localhost:9999");
-        SelenideElement form = $("[class=App_appContainer__3jRx1]");
+
+        SelenideElement form = $("form");
         form.$("[data-test-id=name] input").setValue("Васильевич Василий");
         form.$("[data-test-id=phone] input").setValue("+79270000000");
         form.$("[data-test-id=agreement]").click();
